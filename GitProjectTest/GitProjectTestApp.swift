48d799d222b57adec49b74a6eed19586f530e045
Mickaël MAS <appiwedia@gmail.com>
2021-06-11 11:40:40 +0200

    Initial Commit

---
+++ GitProjectTest/GitProjectTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectTestApp.swift
+//  GitProjectTest
+//
+//  Created by Mickael Mas on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
